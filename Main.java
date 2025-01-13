2dc97b37cf91c02a8faacfa1f2633d30688eb185
Mjijon2345 <121265582+Mjijon2345@users.noreply.github.com>
2025-01-13 01:35:23 +0000

    Aplicar mejora a la interfaz

--- Main.java
+++ Main.java
@@ -2,8 +2,11 @@ public class Main {
     public static void main(String[] args) {
         Despedida despedida = new Despedida();
         System.out.println(despedida.generarDespedida("Mundo"));
+<<<<<<< HEAD
         System.out.println("***************");
         System.out.println("* JAVA PROJECT *");
         System.out.println("***************");
+=======
+>>>>>>> feature/despedida-personalizada
     }
 }
