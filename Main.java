6f01f06b956e3c321d6a5fae22aec0701358a857
Mjijon2345 <121265582+Mjijon2345@users.noreply.github.com>
2025-01-13 01:28:23 +0000

    Agregar saludo multilingüe

--- Main.java
+++ Main.java
@@ -5,5 +5,9 @@ public class Main {
         System.out.println("***************");
         System.out.println("* JAVA PROJECT *");
         System.out.println("***************");
+        Saludo saludo = new Saludo();
+        System.out.println(saludo.generarSaludo("Mundo", "en"));
+        System.out.println(saludo.generarSaludo("Mundo", "es"));
+        System.out.println(saludo.generarSaludo("Mundo", "fr"));
     }
 }
